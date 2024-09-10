a0796631ec2be36a4341b79e056d5c35e051a328
Mykola Solianko <mykola_solianko@epam.com>
2024-09-10 14:41:07 +0300

    Add main
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <iostream>
+
+#include <Poco/Util/ServerApplication.h>
+
+#include "app/app.hpp"
+
+/***********************************************************************************************************************
+ * Main
+ **********************************************************************************************************************/
+
+POCO_SERVER_MAIN(App);
